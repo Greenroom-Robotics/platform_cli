8c201532cbbb950480af4134869bbf058995be6f
David Revay <daverevay@gmail.com>
2022-09-18 22:59:13 +0200

    feat: add release ci

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    branches: ["main"],
+    plugins: [
+        ["@semantic-release/commit-analyzer", { "preset": "conventionalcommits" }],
+        ["@semantic-release/release-notes-generator", { "preset": "conventionalcommits" }],
+        "@semantic-release/changelog",
+        [
+            "@semantic-release/exec",
+            {
+              "prepareCmd": "echo __version__ = \"${nextRelease.version}\" > ./platform_cli/__init__.py",
+            }
+        ],
+        "@semantic-release/github",
+        "@semantic-release/git"
+    ],
+};
