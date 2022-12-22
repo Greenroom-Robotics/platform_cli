3f0446d0e1749df8e1c7dd2200b77a70ec657841
David Revay <daverevay@gmail.com>
2022-12-22 12:57:21 +1100

    feat: add support for releasing single packages

--- platform_cli/groups/release.py
+++ platform_cli/groups/release.py
@@ -14,7 +14,6 @@ from python_on_whales.components.buildx.imagetools.models import Manifest
 from platform_cli.groups.base import PlatformCliGroup
 from platform_cli.helpers import echo, call
 
-PACKAGES_DIRECTORY = "packages"
 DEBS_DIRECTORY = "debs"
 DOCKER_REGISTRY = "localhost:5000"
 
@@ -41,10 +40,10 @@ def get_releaserc(changelog: bool):
                     "publishCmd": "platform release deb-publish",
                 },
             ],
-            [
-                "@semantic-release/github",
-                {"assets": [{"path": "**/*.deb"}], "successComment": False},
-            ],
+            # [
+            #     "@semantic-release/github",
+            #     # {"assets": [{"path": "**/*.deb"}], "successComment": False},
+            # ],
         ],
     }
     if changelog:
@@ -60,33 +59,73 @@ def get_releaserc(changelog: bool):
 
 
 class Release(PlatformCliGroup):
+    def get_package_xmls(self):
+        """Returns all package.xmls ignoring those inside node_modules"""
+        return [Path(p) for p in Path.cwd().glob("**/package.xml") if "node_modules" not in str(p)]
+
+    def _write_root_package_json(self, src: Path):
+        dest = Path.cwd() / "package.json"
+        with open(src) as f:
+            # Set the package_name to be the package.xml package name or the name of the cwd
+            package_json = json.load(f)
+            package_xml_path = Path.cwd() / "package.xml"
+            package_name = (
+                self._get_package_name_from_package_xml(package_xml_path)
+                if package_xml_path.exists()
+                else Path.cwd().name
+            )
+            package_json["name"] = package_name
+            with open(dest, "w") as f:
+                json.dump(package_json, f, indent=4)
+
+    def _write_package_json(self, dest: Path, package_name: str):
+        package_json = {"name": package_name, "version": "0.0.0", "license": "UNLICENSED"}
+        echo(f"Writing {dest}", "blue")
+        with open(dest, "w") as f:
+            json.dump(package_json, f, indent=4)
+
+    def _write_root_yarn_lock(self, src: Path):
+        dest = Path.cwd() / "yarn.lock"
+        shutil.copyfile(src, dest)
+
+    def _get_package_name_from_package_xml(self, package_xml_path: Path) -> str:
+        """Reads the name from a package.xml"""
+        package_xml = ET.parse(package_xml_path)
+        root = package_xml.getroot()
+        package_name: str = root.find("name").text  # type: ignore
+        return package_name
+
     def _generate_package_jsons_for_each_package(self):
         """
         This will generate a fake package.json next to any package.xml.
         This is done as a hack so semantic-release can be used to release the package.
         """
-        echo(f"Looking in ./{PACKAGES_DIRECTORY} for package.xml files", "blue")
-        package_xmls = glob(str(Path.cwd() / f"{PACKAGES_DIRECTORY}/**/package.xml"))
+        package_xmls = self.get_package_xmls()
 
         echo(f"Total found: {len(package_xmls)}", "blue")
         for package_xml_path in package_xmls:
-            package_xml = ET.parse(package_xml_path)
-            root = package_xml.getroot()
-            package_name = root.find("name").text  # type: ignore
-            package_json = {"name": package_name, "version": "0.0.0", "license": "UNLICENSED"}
-            package_json_path = str(Path(package_xml_path).parent / "package.json")
+            package_name = self._get_package_name_from_package_xml(package_xml_path)
+            package_json_path = Path(package_xml_path).parent / "package.json"
+            if not package_json_path.exists():
+                self._write_package_json(package_json_path, package_name)
 
-            echo(f"Writing {package_json_path}", "blue")
-            f = open(package_json_path, "w")
-            f.write(json.dumps(package_json, indent="    "))
-            f.close()
+    def _check_parents_for_file(self, filename: str) -> Path:
+        """Checks each parent directory for a file"""
+        current_path = Path.cwd()
+        while current_path.exists():
+            file_path = current_path / filename
+            if file_path.exists():
+                return file_path.parent
+            current_path = current_path.parent
+
+        raise Exception(f"Could not find {filename} in any parent directory")
 
     def _get_package_info(self) -> PackageInfo:
         """
         Returns the package info for the current working directory.
         This assumes the cwd is a package. It will find out the name of the platform module.
 
-        File structure should be:
+        File structure should be something like:
         platform_module/packages/package_name
 
         eg)
@@ -94,7 +133,7 @@ class Release(PlatformCliGroup):
         """
         package_path = Path.cwd()
         package_name = package_path.name
-        platform_module_path = (Path.cwd() / "../..").resolve()
+        platform_module_path = self._check_parents_for_file("Dockerfile")
         platform_module_name = platform_module_path.name
 
         return PackageInfo(
@@ -107,9 +146,7 @@ class Release(PlatformCliGroup):
     def _build_deb_in_docker(
         self,
         version: str,
-        platform_module_name: str,
-        platform_module_path: Path,
-        package_name: str,
+        package_info: PackageInfo,
         docker_image_name: str,
         architecture: str,
         image_manifests: Manifest,
@@ -130,8 +167,16 @@ class Release(PlatformCliGroup):
         echo(f"Docker image manifest {image_for_docker_platform}", "blue")
         docker_plaform = f"linux/{architecture}"
 
-        host_debs_path = platform_module_path / PACKAGES_DIRECTORY / package_name / DEBS_DIRECTORY
-        docker_debs_path = f"/home/ros/{platform_module_name}/{PACKAGES_DIRECTORY}/{package_name}/{DEBS_DIRECTORY}"
+        package_relative_to_platform_module = package_info.package_path.relative_to(
+            package_info.platform_module_path
+        )
+        host_debs_path = package_info.package_path / DEBS_DIRECTORY
+        docker_working_dir = (
+            Path("/home/ros/")
+            / package_info.platform_module_name
+            / package_relative_to_platform_module
+        )
+        docker_debs_path = docker_working_dir / DEBS_DIRECTORY
 
         # Make the .debs directory on the host, otherwise docker will make it with root permissions!
         host_debs_path.mkdir(exist_ok=True)
@@ -146,7 +191,7 @@ class Release(PlatformCliGroup):
                 f"source /home/ros/.profile && platform pkg build --version {version} --output {DEBS_DIRECTORY} && platform pkg clean",
             ],
             interactive=True,
-            workdir=f"/home/ros/{platform_module_name}/{PACKAGES_DIRECTORY}/{package_name}",
+            workdir=docker_working_dir,
             volumes=[
                 # We only mount the /debs directory for each package
                 (host_debs_path, docker_debs_path)
@@ -166,12 +211,8 @@ class Release(PlatformCliGroup):
             echo("Copying package.json and yarn.lock to root and installing deps...", "blue")
             asset_dir = Path(__file__).parent.parent / "assets"
 
-            dest_path_package_json = Path.cwd() / "package.json"
-            dest_path_yarn_lock = Path.cwd() / "yarn.lock"
-
-            shutil.copyfile(asset_dir / "package.json", dest_path_package_json)
-            shutil.copyfile(asset_dir / "yarn.lock", dest_path_yarn_lock)
-
+            self._write_root_package_json(asset_dir / "package.json")
+            self._write_root_yarn_lock(asset_dir / "yarn.lock")
             self._generate_package_jsons_for_each_package()
 
             call("yarn install --frozen-lockfile")
@@ -189,14 +230,26 @@ class Release(PlatformCliGroup):
         )
         def create(changelog: bool, args: List[str]):  # type: ignore
             """Creates a release of the platform module package. See .releaserc for more info"""
-            print(changelog)
             releaserc = get_releaserc(changelog)
             dest_path_releaserc = Path.cwd() / ".releaserc"
             with open(dest_path_releaserc, "w+") as f:
                 f.write(json.dumps(releaserc, indent=4))
 
             args_str = " ".join(args)
-            call(f"yarn multi-semantic-release {args_str}")
+
+            # Check to see if the root has a package.xml
+            package_xml_path = Path.cwd() / "package.xml"
+            if package_xml_path.exists():
+                echo(
+                    "Root has a package.xml, running semantic-release for a single package", "blue"
+                )
+                call(f"yarn semantic-release {args_str}")
+            else:
+                echo(
+                    "Root has no package.xml, running semantic-release for multiple packages",
+                    "blue",
+                )
+                call(f"yarn multi-semantic-release {args_str}")
 
         @release.command(name="deb-prepare")
         @click.option("--version", type=str, help="The version to call the debian", required=True)
@@ -252,14 +305,6 @@ class Release(PlatformCliGroup):
 
             docker.buildx.use("platform")
 
-            docker_file_exists = os.path.isfile("../../Dockerfile")
-            if not docker_file_exists:
-                echo(
-                    f"Dockerfile does not exist in {package_info.platform_module_path}. This must be run from a package directory.",
-                    "red",
-                )
-                exit(1)
-
             # Build the images for arm and amd using buildx
             docker.buildx.build(
                 "../../",
@@ -279,9 +324,7 @@ class Release(PlatformCliGroup):
                 try:
                     self._build_deb_in_docker(
                         version=version,
-                        platform_module_name=package_info.platform_module_name,
-                        platform_module_path=package_info.platform_module_path,
-                        package_name=package_info.package_name,
+                        package_info=package_info,
                         docker_image_name=docker_image_name,
                         architecture=architecture,
                         image_manifests=image_manifests,
