84305a37affb8059b81ee9c637fd2736fd7a5973
Russ Webber <russ.webber@greenroomrobotics.com>
2022-12-22 16:43:11 +1100

    fix

--- platform_cli/groups/ros.py
+++ platform_cli/groups/ros.py
@@ -57,7 +57,6 @@ class Ros(PlatformCliGroup):
             echo("Testing packages...", 'green')
             p = call(f"colcon test --merge-install --install-base /opt/greenroom/{env['PLATFORM_MODULE']} {args_str}", abort=False)
             call(f"colcon test-result --all --verbose {args_str}", abort=False)
-            call("cat log/latest_test/logger_all.log")
             exit(p.returncode)
 
         @ros.command(name="install_poetry_deps")
